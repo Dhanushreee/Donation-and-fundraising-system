3f4469442fe5137d97ac4cb5f727bbced239fc17
Dhanushreee <117502544+Dhanushreee@users.noreply.github.com>
2023-12-13 21:27:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './Signup';
+import Signup from './Signup';
+
+function App() {
+  return (
+    <div className="App">
+    <Signup name="Name:" Password="Password" Phone="Phone" />
+    </div>
+  );
+}
+
+export default App;
